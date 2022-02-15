ee6f1509d9bc8e9da5b319472bdf049ad7536c11
NhanHcmus20 <81221996+NhanHcmus20@users.noreply.github.com>
2022-02-15 14:49:56 +0700

    hw

---
+++ BT_buoi7/index.js
@@ -0,0 +1,71 @@
+let n = 10;
+console.log("bài 0");
+
+function checkPrime(n) {
+    if (n < 2){
+        return false;
+    }
+    for (let index = 2; index < n; index++) {
+        if(n % i == 0){
+            return false;
+        }
+        
+    }
+    return true
+}
+
+function checkN(n){
+    if (checkPrime(n)) {
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
+
+console.log('bài 1');
+
+function print1ToN(n) {
+    for (let index = 0; index < n; index++) {
+        console.log(i);
+    }
+}
+
+console.log('bài 2')
+
+function printOdd(n) {
+    for (let index = 0; index < n; index++) {
+        if(i % 2 == 1){
+            console.log(i);
+        }
+    }
+}
+
+console.log('Bài 3')
+
+function printEven(n) {
+    for (let index = 0; index < n; index++) {
+        if(i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+
+console.log('Bài 4')
+
+function printPrime(n) {
+    for (let index = 0; index < n; index++) {
+        if(checkPrime(i)){
+            console.log(i);
+        }
+    }
+}
+
+console.log('Bài 5')
+
+function getSum(n) {
+    let sum = 0;
+    for (let index = 0; index < n; index++) {
+        sum += i;
+    }
+    return sum;
+}
